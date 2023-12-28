dbaa7b5b492b52e56648d6d6698b1c39f5430db2
aaditeeiche <aaditee.iche@gmail.com>
2023-12-29 04:20:05 +0530

    Week 4 completed

--- Week 4/experiment3/application.py
+++ Week 4/experiment3/application.py
@@ -2,6 +2,7 @@
 from flask import Flask
 # Import this to use the Jinja2 implemented in Flask modules
 from flask import render_template
+from flask import request
 
 # Create the app
 app = Flask(__name__)
@@ -9,7 +10,13 @@ app = Flask(__name__)
 # default method added is methods=['GET']
 @app.route("/hello", methods=['GET', 'POST'])
 def hello_world():
-    return render_template("submit_page.html")
+    if request.method == 'GET':
+        return render_template("submit_page.html")
+    elif request.method == 'POST':
+        username = request.form["username"]
+        return render_template("display_details.html", display_name=username)
+    else:
+        print("Error Check")    
 
 if __name__== '__main__':
     app.debug = True
